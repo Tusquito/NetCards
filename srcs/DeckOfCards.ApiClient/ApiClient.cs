085afb0bb3714e8146656688e98a03c7726e961e
Tusquito <tusquitoweb@gmail.com>
2021-02-21 10:02:41 +0100

    Add partial deck creation & rename main prokect

--- srcs/DeckOfCards.ApiClient/ApiClient.cs
+++ srcs/DeckOfCards.ApiClient/ApiClient.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using DeckOfCards.Client.Builders;
+using DeckOfCards.Client.Enums;
 using DeckOfCards.Client.Extensions;
 using DeckOfCards.Client.Responses;
 using Newtonsoft.Json;
@@ -17,9 +20,9 @@ namespace DeckOfCards.Client
             HttpClient.BaseAddress = new Uri(ApiRouteExtensions.BASE_ROUTE);
         }
 
-        public async Task<ShuffleDeckApiResponse> CreateNewDeckAsync(int deckCount = 1)
+        public async Task<ShuffleDeckApiResponse> CreateNewDeckAsync(int deckCount = 1, bool jokersEnabled = true)
         {
-            var response = await HttpClient.GetAsync(string.Format(ApiRouteExtensions.NEW_DECK_ROUTE, deckCount));
+            var response = await HttpClient.GetAsync(string.Format(ApiRouteExtensions.NEW_DECK_ROUTE, deckCount, jokersEnabled.ToString().ToLower()));
             try
             {
                 response.EnsureSuccessStatusCode();
@@ -82,6 +85,32 @@ namespace DeckOfCards.Client
             catch (HttpRequestException e)
             {
                 return new DrawCardsApiResponse
+                {
+                    StatusCode = response.StatusCode,
+                    ResponseMessage = e.Message
+                };
+            }
+        }
+
+        public async Task<ShuffleDeckApiResponse> CreateNewPartialDeckAsync(IEnumerable<(char, SuitType)> cardsCodes)
+        {
+            var routeParam = cardsCodes.Aggregate("", (x, y) => $"{x},{y.Item1}{y.Item2.ToString().Take(1)}");
+            var response =
+                await HttpClient.GetAsync(string.Format(ApiRouteExtensions.NEW_PARTIAL_DECK_ROUTE, routeParam));
+            try
+            {
+                response.EnsureSuccessStatusCode();
+                return new ApiResponseBuilder<ShuffleDeckApiResponse>(
+                        JsonConvert.DeserializeObject<ShuffleDeckApiResponse>(
+                            await response.Content.ReadAsStringAsync()))
+                    .WithResponseMessage("Success")
+                    .WithStatusCode(response.StatusCode)
+                    .Build();
+
+            }
+            catch (HttpRequestException e)
+            {
+                return new ShuffleDeckApiResponse
                 {
                     StatusCode = response.StatusCode,
                     ResponseMessage = e.Message
