e2a6e5ccc2054279fe19a6b63845b25c9062165f
Tusquito <tusquitoweb@gmail.com>
2021-03-03 01:50:15 +0100

    Automatically remove unused decks

--- NetCards.Api/srcs/NetCards.Api.Core/Managers/DeckManager.cs
+++ NetCards.Api/srcs/NetCards.Api.Core/Managers/DeckManager.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
 using NetCards.Api.Core.Entities;
 
 namespace NetCards.Api.Core.Managers
 {
-    public class DeckManager : IManager<string, DeckInformation>
+    public class DeckManager : BackgroundService, IManager<string, DeckInformation>
     {
         private readonly ConcurrentDictionary<string, DeckInformation> _decks = new();
+        private readonly TimeSpan _updateDelay = TimeSpan.FromSeconds(Convert.ToInt32(Environment.GetEnvironmentVariable("DECK_MANAGER_UPDATE_DELAY") ?? "60"));
+        private readonly TimeSpan _deckDuration = TimeSpan.FromMinutes(Convert.ToInt32(Environment.GetEnvironmentVariable("DECK_MANAGER_DECK_DURATION") ?? "60"));
 
         public bool Exists(string key)
         {
@@ -16,6 +22,7 @@ namespace NetCards.Api.Core.Managers
 
         public void AddOrUpdate(DeckInformation obj)
         {
+            obj.LastUpdate = DateTime.UtcNow;
             _decks[obj.Id] = obj;
         }
 
@@ -26,6 +33,7 @@ namespace NetCards.Api.Core.Managers
 
         public DeckInformation Get(string key)
         {
+            _decks[key].LastUpdate = DateTime.UtcNow;
             return _decks[key];
         }
 
@@ -37,6 +45,19 @@ namespace NetCards.Api.Core.Managers
         public void RemoveAll()
         {
             _decks.Clear();
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var decksToDelete = _decks.Where(p => p.Value.LastUpdate + _deckDuration < DateTime.UtcNow);
+                foreach (var (deckId, deck) in decksToDelete)
+                {
+                    Remove(deckId);
+                }
+                await Task.Delay(_updateDelay, stoppingToken);
+            }
         }
     }
 
